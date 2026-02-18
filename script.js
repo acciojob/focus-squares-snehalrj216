692d0442ac2d3483df511f42d1fe61103b73fad3
Snehal Jadhav <145901611+snehalrj216@users.noreply.github.com>
2026-02-18 20:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let squares = document.querySelectorAll(".square");
+
+squares.forEach(square => {
+
+    square.addEventListener("mouseenter", function() {
+
+        squares.forEach(otherSquare => {
+            if (otherSquare !== square) {
+                otherSquare.style.backgroundColor = "#6F4E37"; // Coffee
+            }
+        });
+
+    });
+
+    square.addEventListener("mouseleave", function() {
+
+        squares.forEach(sq => {
+            sq.style.backgroundColor = "#E6E6FA"; // Reset to Lavender
+        });
+
+    });
+
+});
